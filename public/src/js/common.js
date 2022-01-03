ebff4e25770079af3022b67e219b16da8c5e6790
kangtaewan <bras1717@naver.com>
2022-01-03 23:49:44 +0900

    main&gnb&footer layout완성

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -3,9 +3,18 @@ let gnbOpen = 'false';
 const menuBtn = document.querySelector('.js-menu-btn');
 const gnb = document.querySelector('.gnb');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click',function(){
-    gnb.classList.add('is-active');
-    header.classList.add('is-active');
-    menuBtn.classList.add('is-active');
+    if(menuBtn.classList.contains('is-active')){
+        gnb.classList.remove('is-active');
+        header.classList.remove('is-active');
+        menuBtn.classList.remove('is-active');
+        body.classList.remove('is-active');
+    }else{
+        gnb.classList.add('is-active');
+        header.classList.add('is-active');
+        menuBtn.classList.add('is-active');
+        body.classList.add('is-active');
+    }
 })
